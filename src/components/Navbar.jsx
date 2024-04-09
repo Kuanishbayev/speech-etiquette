8df0e3b937ecc03a47a4dcae5f13a648931a3b3e
Ernazar <kuanishbayevyernazar@gmail.com>
2024-04-09 14:36:07 +0500

    navbar position and menu display fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     window.addEventListener('scroll', changeColor)
   return (
-    <nav className={color ? 'relative py-5 sticky top-0 ease-in duration-300 bg-slate-50 z-10' : 'relative py-5 sticky top-0 z-10 ease-in duration-300'}>
+    <nav className={color ? 'relative py-5 top-0 ease-in duration-300 bg-slate-50 z-10' : 'relative py-5 top-0 z-10 ease-in duration-300'}>
         <div className='max-w-[1280px] mx-auto flex justify-between items-center px-4 lg:px-0'>
             <div className="logo">
                 <img src="./se.png" className="w-10" alt="logo" />
@@ -36,7 +36,7 @@ const Navbar = () => {
             <div className="lg:hidden" onClick={() => setMenu(prev => !prev)}>
                 {menu ? <IoClose size={30} /> : <IoMenu size={30} />}
             </div>
-                <ul className={menu ? 'block ease-in-out duration-200 font-bold flex flex-col gap-4 items-center justify-center py-10 absolute top-full right-0 bg-stone-600 w-full' : 'hidden'}>
+                <ul className={menu ? 'ease-in-out duration-200 font-bold flex flex-col gap-4 items-center justify-center py-10 absolute top-full right-0 bg-stone-600 w-full' : 'hidden'}>
                     <li>
                         <a href="#">Home</a>
                     </li>
