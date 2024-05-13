c563b228a678ae5e6a7449aacde5a24da52a367e
Ernazar <kuanishbayevyernazar@gmail.com>
2024-05-13 15:24:46 +0500

    added token to POST request in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     const res = await fetch('https://speech.orfo-qareken.uz/api/login', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+              'Authorization': `Bearer ${token}`
             },
             body: JSON.stringify({
               'email': data.email,
