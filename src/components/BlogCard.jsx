117ffc9bad95fabf5c90e2e74850b474937406f4
Ernazar <kuanishbayevyernazar@gmail.com>
2024-05-18 13:59:41 +0500

    BlogCard title line-clamp bug's was fixed.

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -10,7 +10,7 @@ const BlogCard = ({id, image, title, body, uploadedDate}) => {
           <img className="h-full object-contain mx-auto" src={image} alt="thumbnail" />
         </div>
         <div className="content">
-          <p className='group-hover/item:text-indigo-800 transition ease-in-out duration-150 font-bold pb-6 pt-2 md:text-xl lg:text-lg line-clamp-2'>{title}</p>
+          <p className='group-hover/item:text-indigo-800 transition ease-in-out duration-150 font-bold mb-6 mt-2 md:text-xl lg:text-lg line-clamp-2'>{title}</p>
           {body && <div className='line-clamp-5' dangerouslySetInnerHTML={{__html:body}} />}
           {uploadedDate && <p className='lg:pt-2 flex items-center gap-2 justify-end text-gray-500'><LuCalendar />{uploadedDate.split('T')[0].replaceAll('-', '.').split('.').reverse().join('.')}</p>}
         </div>
--- src/components/admin/BlogCard.jsx
+++ src/components/admin/BlogCard.jsx
@@ -8,7 +8,7 @@ const BlogCard = ({image, title, body, uploadedDate, children}) => {
           <img className="h-full object-contain mx-auto" src={image} alt="thumbnail" />
       </div>
       <div className="content">
-        <p className='font-bold pb-6 pt-2 md:text-xl lg:text-lg line-clamp-3'>{title}</p>
+        <p className='font-bold mb-6 mt-2 md:text-xl lg:text-lg line-clamp-3'>{title}</p>
         {body && <div className='line-clamp-5' dangerouslySetInnerHTML={{__html:body}} />}
         <p className='lg:pt-2 flex items-center gap-2 justify-end text-gray-500'><LuCalendar />{uploadedDate.split('T')[0].replaceAll('-', '.').split('.').reverse().join('.')}</p>
       </div>
